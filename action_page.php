8f96783c7ab745c245854f14c27a76efca5824d4
PLEBMASTER <34640571+PLEBMASTER@users.noreply.github.com>
2017-12-20 11:09:31 -0800

    Update action_page.php

--- action_page.php
+++ action_page.php
@@ -3,12 +3,7 @@ if($_POST['submit']) {
 	  
 		$name = substr(htmlspecialchars(trim($_POST['name'])), 0, 1000);
 		$email =  substr(htmlspecialchars(trim($_POST['email'])), 0, 1000);
-        $telefon =  substr(htmlspecialchars(trim($_POST['telefon'])), 0, 1000000);
-        $mesaj =  substr(htmlspecialchars(trim($_POST['mesaj'])), 0, 1000000);
-		$header = "From: ". $name . "--> Email: <" . $email . "> Telefon: <" . $telefon . ">\r\n";
-        $to = '';
-        
-mail($to, $header, 'Your message: '.$mesaj);
+		$header = "From: ". $name . "--> Email: <" . $email . "> 
 		
 }
  
