2ad985f692533381c791f35f4f4fc1ebab8d3859
PLEBMASTER <34640571+PLEBMASTER@users.noreply.github.com>
2017-12-22 13:16:28 -0800

    Update google_script_form_ajax.js

--- google_script_form_ajax.js
+++ google_script_form_ajax.js
@@ -1,5 +1,5 @@
 var $form = $('form#test-form'),
-    url = 'https://script.google.com/macros/s/abcdefghijklmnopqrstuvwxyz1234567890/exec'
+    url = 'https://script.google.com/macros/s/AKfycbzIGyys0TCY-hc6KvdtE0SKz51iNLcRPulONVKQyjdshxH_8uFM/exec'
 
 $('#submit-form').on('click', function(e) {
   e.preventDefault();
